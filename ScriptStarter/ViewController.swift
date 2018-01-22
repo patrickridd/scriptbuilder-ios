848ed21334b571db3f8aa10fbc8763044db12142
Patrick Ridd <patrick.ridd@stgconsulting.com>
2018-01-22 10:08:43 -0700

    Initial Commit

---
+++ ScriptStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScriptStarter
+//
+//  Created by Patrick Ridd (patrick.ridd@stgconsulting.com) on 1/19/18.
+//  Copyright © 2018 patrickridd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ScriptStarterTests/ScriptStarterTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScriptStarterTests.swift
+//  ScriptStarterTests
+//
+//  Created by Patrick Ridd (patrick.ridd@stgconsulting.com) on 1/19/18.
+//  Copyright © 2018 patrickridd. All rights reserved.
+//
+
+import XCTest
+@testable import ScriptStarter
+
+class ScriptStarterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
