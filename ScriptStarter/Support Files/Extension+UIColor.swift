6b13c572395d3cda7dbacf025bec97d6edcf85f2
Patrick Ridd <patrick.ridd@stgconsulting.com>
2018-01-29 18:16:14 -0700

    Created new color for the screenLightGray. Changed the opacity on the flamenco color and it looks way better for the placeholder text. Looking good!

--- ScriptStarter/Support Files/Extension+UIColor.swift
+++ ScriptStarter/Support Files/Extension+UIColor.swift
@@ -21,6 +21,13 @@ extension UIColor {
     static var screenDarkMediumGray: UIColor {
         return UIColor(red: 0.37, green: 0.37, blue: 0.37, alpha: 1.0)
     }
+    static var screenMediumGray: UIColor {
+        return UIColor(red: 0.88, green: 0.88, blue: 0.88, alpha: 1.0)
+    }
+    
+    static var screenLightGray: UIColor {
+        return UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
+    }
     
     static var screenLightBlue: UIColor {
         return UIColor(red: 0.53, green: 0.72, blue: 0.85, alpha: 1.0)
@@ -30,9 +37,7 @@ extension UIColor {
         return UIColor(red: 0.83, green: 0.89, blue: 0.88, alpha: 1.0)
     }
     
-    static var screenLightGray: UIColor {
-        return UIColor(red: 0.88, green: 0.88, blue: 0.88, alpha: 1.0)
-    }
+  
     
     static var flamingoRed: UIColor {
         return UIColor(red: 0.87, green: 0.39, blue: 0.30, alpha: 1.0)
@@ -43,6 +48,6 @@ extension UIColor {
     }
     
     static var flamenco: UIColor {
-        return UIColor(red: 0.92, green: 0.50, blue: 0.31, alpha: 1.0)
+        return UIColor(red: 0.92, green: 0.50, blue: 0.31, alpha: 0.50)
     }
 }
