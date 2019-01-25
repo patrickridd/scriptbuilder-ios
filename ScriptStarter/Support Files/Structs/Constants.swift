72c9ea0fca6cb45206e86e75629e663b27ccd6ec
Patrick Ridd <patrick.ridd@stgconsulting.com>
2019-01-25 16:51:28 -0700

    Created Constants file where we place our static keys and identifiers

---
+++ ScriptStarter/Support Files/Structs/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ScriptStarter
+//
+//  Created by Patrick Ridd (patrick.ridd@stgconsulting.com) on 1/25/19.
+//  Copyright © 2019 patrickridd. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    // Keys
+    static let shouldScheduleInterstitial: String = "shouldScheduleAd"
+}
