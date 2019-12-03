1b8cd610da8109886f69456455975f9b17207a75
Patrick Ridd <patrick.ridd@stgconsulting.com>
2019-12-03 14:14:12 -0700

    Removed addBodyText method because I'm not using it.

--- ScriptStarter/Support Files/Classes/PdfHelper.swift
+++ ScriptStarter/Support Files/Classes/PdfHelper.swift
@@ -320,56 +320,5 @@ class PdfHelper {
     }
     
     
-//    func addBodyText(pageRect: CGRect, textTop: CGFloat) {
-//        let textFont = UIFont.systemFont(ofSize: 12.0, weight: .regular)
-//        // 1
-//        let paragraphStyle = NSMutableParagraphStyle()
-//        paragraphStyle.alignment = .natural
-//        paragraphStyle.lineBreakMode = .byWordWrapping
-//        // 2
-//        let textAttributes = [
-//            NSAttributedString.Key.paragraphStyle: paragraphStyle,
-//            NSAttributedString.Key.font: textFont
-//        ]
-//        let attributedText = NSAttributedString(
-//            string: body,
-//            attributes: textAttributes
-//        )
-//        // 3
-//        let textRect = CGRect(
-//            x: 10,
-//            y: textTop,
-//            width: pageRect.width - 20,
-//            height: pageRect.height - textTop - pageRect.height / 5.0
-//        )
-//        attributedText.draw(in: textRect)
-//    }
-//
-//    func addTitle(pageRect: CGRect) -> CGFloat {
-//        // 1
-//        let titleFont = UIFont.systemFont(ofSize: 18.0, weight: .bold)
-//        // 2
-//        let titleAttributes: [NSAttributedString.Key: Any] =
-//            [NSAttributedString.Key.font: titleFont]
-//        // 3
-//        let attributedTitle = NSAttributedString(
-//            string: title,
-//            attributes: titleAttributes
-//        )
-//        // 4
-//        let titleStringSize = attributedTitle.size()
-//        // 5
-//        let titleStringRect = CGRect(
-//            x: (pageRect.width - titleStringSize.width) / 2.0,
-//            y: 36,
-//            width: titleStringSize.width,
-//            height: titleStringSize.height
-//        )
-//        // 6
-//        attributedTitle.draw(in: titleStringRect)
-//        // 7
-//        return titleStringRect.origin.y + titleStringRect.size.height
-//    }
-
 
 }
