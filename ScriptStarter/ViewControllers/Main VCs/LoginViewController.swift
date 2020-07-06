d9d90c0c8ec72d20d59745823237fc6b19afb15f
Patrick Ridd <patrick.ridd@stgconsulting.com>
2020-07-06 16:18:35 -0700

    LoginViewController
    • Setup the apple SignIn button
    • Conformed to the ASAuthorizationControllerDelegate and created a firebase authenticated user with the results
    • Reformatted code
    • Sent user to screenplay screen as soon as they login/sign up

--- ScriptStarter/ViewControllers/Main VCs/LoginViewController.swift
+++ ScriptStarter/ViewControllers/Main VCs/LoginViewController.swift
@@ -14,9 +14,11 @@ import FBSDKLoginKit
 import GoogleSignIn
 import Firebase
 import MBProgressHUD
+import AuthenticationServices
+import CryptoKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, ASAuthorizationControllerPresentationContextProviding {
+    
     @IBOutlet weak var authenticationStackView: UIStackView!
     @IBOutlet weak var facebookButton: UIButton!
     @IBOutlet weak var googleSignInButton: GIDSignInButton!
@@ -43,42 +45,121 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            // Setup Facebook sign in buttons
-            self.facebookButton.addTarget(self,
-                                     action: #selector(self.facebookButtonTapped),
-                                     for: .touchUpInside)
-            // Google Sign-in
-            GIDSignIn.sharedInstance().delegate = self
-            GIDSignIn.sharedInstance()?.presentingViewController = self
-            self.googleSignInButton.style = .iconOnly
-            self.googleSignInButton.colorScheme = .light
-            
-            // Set TextFields Delegate
-            self.emailTextField.delegate = self
-            self.passwordTextField.delegate = self
-            
-            // Sets keyboard observers so we can adjust the textfields
-            self.addKeyBoardObservers()
-            
-            // Add toolbars to be able to dismiss keyboard manually
-            self.addToolBar(textField: self.emailTextField)
-            self.addToolBar(textField: self.passwordTextField)
-            
-            // Setup Tap Gesture to dismiss keyboard
-            let tapGesture = UITapGestureRecognizer(target: self,
-                                                    action: #selector(self.dismissKeyboard))
-            self.view.addGestureRecognizer(tapGesture)
-            tapGesture.cancelsTouchesInView = false // This way the google button will work
-            
-            let strokeTextAttributes: [NSAttributedString.Key : Any] =
-                [NSAttributedString.Key.strokeColor: UIColor.screenLightBlue,
-                 NSAttributedString.Key.foregroundColor: UIColor.white,
-                 NSAttributedString.Key.strokeWidth: 1]
-            self.scriptBuilderLabel.attributedText = NSAttributedString(string: "Script Builder".localized,
-                                                                        attributes: strokeTextAttributes)
+        // Setup Facebook sign in buttons
+        self.facebookButton.addTarget(self,
+                                      action: #selector(self.facebookButtonTapped),
+                                      for: .touchUpInside)
+        // Google Sign-in
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        self.googleSignInButton.style = .standard
+        self.googleSignInButton.colorScheme = .light
+        
+        // Set TextFields Delegate
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        
+        // Sets keyboard observers so we can adjust the textfields
+        self.addKeyBoardObservers()
+        
+        // Add toolbars to be able to dismiss keyboard manually
+        self.addToolBar(textField: self.emailTextField)
+        self.addToolBar(textField: self.passwordTextField)
+        
+        // Setup Tap Gesture to dismiss keyboard
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        tapGesture.cancelsTouchesInView = false // This way the google button will work
+        
+        let strokeTextAttributes: [NSAttributedString.Key : Any] =
+            [NSAttributedString.Key.strokeColor: UIColor.screenLightBlue,
+             NSAttributedString.Key.foregroundColor: UIColor.white,
+             NSAttributedString.Key.strokeWidth: 1]
+        self.scriptBuilderLabel.attributedText = NSAttributedString(string: "Script Builder".localized,
+                                                                    attributes: strokeTextAttributes)
+        if #available(iOS 13.0, *) {
+            setupProviderLoginView()
+        } else {
+            // Fallback on earlier versions
+        }
     }
     
     // MARK: UI Methods
+    @available(iOS 13.0, *)
+    func setupProviderLoginView() {
+        let authorizationButton = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn,
+                                                               authorizationButtonStyle: .whiteOutline)
+        authorizationButton.addTarget(self,
+                                      action: #selector(handleAuthorizationAppleIDButtonPress),
+                                      for: .touchUpInside)
+        self.authenticationStackView.addArrangedSubview(authorizationButton)
+    }
+    
+    // Unhashed nonce.
+    fileprivate var currentNonce: String?
+    
+    @objc
+    @available(iOS 13.0, *)
+    func handleAuthorizationAppleIDButtonPress() {
+        let nonce = randomNonceString()
+         currentNonce = nonce
+         let appleIDProvider = ASAuthorizationAppleIDProvider()
+         let request = appleIDProvider.createRequest()
+         request.requestedScopes = [.fullName, .email]
+         request.nonce = sha256(nonce)
+
+         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+         authorizationController.delegate = self
+         authorizationController.presentationContextProvider = self
+         authorizationController.performRequests()
+    }
+    
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap { return String(format: "%02x", $0) }.joined()
+        
+        return hashString
+    }
+    
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        
+        var charset = Array<String.Element>()
+        let string = "0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._"
+        for char in string {
+            charset.append(char)
+        }
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
     
     func showActivityIndicator() {
         DispatchQueue.main.async {
@@ -124,32 +205,32 @@ class LoginViewController: UIViewController {
         
         showActivityIndicator()
         loginManager.logIn(permissions: [.publicProfile], viewController: self) { [weak self] loginResult in
-                switch loginResult {
-                case .failed:
-                    self?.hideActivityIndicator(success: false,
-                                                completion: nil)
-                case .cancelled:
+            switch loginResult {
+            case .failed:
+                self?.hideActivityIndicator(success: false,
+                                            completion: nil)
+            case .cancelled:
+                self?.hideActivityIndicator(success: false)
+                
+            case .success(granted: _, declined: _, token: _):
+                // Login with Firebase
+                guard let tokenString = AccessToken.current?.tokenString else {
                     self?.hideActivityIndicator(success: false)
-        
-                case .success(granted: _, declined: _, token: _):
-                    // Login with Firebase
-                    guard let tokenString = AccessToken.current?.tokenString else {
+                    print("Facebook Token String nil")
+                    return
+                }
+                let credential = FacebookAuthProvider.credential(withAccessToken: tokenString)
+                Auth.auth().signIn(with: credential) { [weak self] (user, error) in
+                    if let error = error {
                         self?.hideActivityIndicator(success: false)
-                        print("Facebook Token String nil")
+                        print(error.localizedDescription)
                         return
                     }
-                    let credential = FacebookAuthProvider.credential(withAccessToken: tokenString)
-                    Auth.auth().signIn(with: credential) { [weak self] (user, error) in
-                        if let error = error {
-                            self?.hideActivityIndicator(success: false)
-                            print(error.localizedDescription)
-                            return
-                        }
-                        self?.hideActivityIndicator(success: true, completion: {
-                            self?.presentScreenPlayCollection()
-                        })
-                    }
+                    self?.hideActivityIndicator(success: true, completion: {
+                        self?.presentScreenPlayCollection()
+                    })
                 }
+            }
         }
     }
     
@@ -157,7 +238,7 @@ class LoginViewController: UIViewController {
         guard let email = emailTextField.text,
             let password = passwordTextField.text else {
                 self.present(UIAlertControllers.emailAuthenticationError(message: "Complete both fields".localized),
-                                                                         animated: true)
+                             animated: true)
                 return
         }
         
@@ -194,33 +275,33 @@ class LoginViewController: UIViewController {
         
         let confirmAction = UIAlertAction(title: "OK".localized,
                                           style: .default) { [weak alertController, weak self] _ in
-            guard let alertController = alertController,
-            let textField = alertController.textFields?.first else { return }
-            //compare the current password and do action here
-                        
-            guard let email = textField.text, email != "" else {
-                self?.present(UIAlertControllers.emailAuthenticationError(message: "Please enter your email".localized),
-                             animated: true,
-                             completion: nil)
-                return
-            }
-            self?.showActivityIndicator()
-            Auth.auth().sendPasswordReset(withEmail: email) { [weak self] error in
-                
-                // If error exists Alert User
-                if let error = error {
-                    self?.hideActivityIndicator(success: false)
-                    self?.present(UIAlertControllers.emailAuthenticationError(message: error.localizedDescription),
-                                  animated: true,
-                                  completion: nil)
-                    // Else let user know the password was reset
-                } else {
-                    self?.hideActivityIndicator(success: true)
-                    self?.present(UIAlertControllers.passwordResetSuccess(email: email),
-                                  animated: true,
-                                  completion: nil)
-                }
-            }
+                                            guard let alertController = alertController,
+                                                let textField = alertController.textFields?.first else { return }
+                                            //compare the current password and do action here
+                                            
+                                            guard let email = textField.text, email != "" else {
+                                                self?.present(UIAlertControllers.emailAuthenticationError(message: "Please enter your email".localized),
+                                                              animated: true,
+                                                              completion: nil)
+                                                return
+                                            }
+                                            self?.showActivityIndicator()
+                                            Auth.auth().sendPasswordReset(withEmail: email) { [weak self] error in
+                                                
+                                                // If error exists Alert User
+                                                if let error = error {
+                                                    self?.hideActivityIndicator(success: false)
+                                                    self?.present(UIAlertControllers.emailAuthenticationError(message: error.localizedDescription),
+                                                                  animated: true,
+                                                                  completion: nil)
+                                                    // Else let user know the password was reset
+                                                } else {
+                                                    self?.hideActivityIndicator(success: true)
+                                                    self?.present(UIAlertControllers.passwordResetSuccess(email: email),
+                                                                  animated: true,
+                                                                  completion: nil)
+                                                }
+                                            }
         }
         alertController.addAction(confirmAction)
         let cancelAction = UIAlertAction(title: "Cancel".localized,
@@ -325,7 +406,7 @@ extension LoginViewController: GIDSignInDelegate {
             return
         }
         let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                          accessToken: authentication.accessToken)
+                                                       accessToken: authentication.accessToken)
         Auth.auth().signIn(with: credential) { [weak self] (user, error) in
             if let error = error {
                 self?.hideActivityIndicator(success: false)
@@ -339,4 +420,48 @@ extension LoginViewController: GIDSignInDelegate {
     
 }
 
-//extension LoginViewController: GIDSignInUIDelegate {}
+@available(iOS 13.0, *)
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+      if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+        guard let nonce = currentNonce else {
+          fatalError("Invalid state: A login callback was received, but no login request was sent.")
+        }
+        guard let appleIDToken = appleIDCredential.identityToken else {
+          print("Unable to fetch identity token")
+          return
+        }
+        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+          print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+          return
+        }
+        // Initialize a Firebase credential.
+        let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                  idToken: idTokenString,
+                                                  rawNonce: nonce)
+        // Sign in with Firebase.
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+            // Error. If error.code == .MissingOrInvalidNonce, make sure
+            // you're sending the SHA256-hashed nonce as a hex string with
+            // your request to Apple.
+                print(error.localizedDescription)
+            return
+          }
+            self.hideActivityIndicator(success: true, completion: {
+            self.presentScreenPlayCollection()
+          })
+        }
+      }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+      // Handle error.
+      print("Sign in with Apple errored: \(error)")
+    }
+}
