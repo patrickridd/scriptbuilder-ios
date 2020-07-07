2058c455fa5c91fab40f5bf53471e2003dc3c2ea
Patrick Ridd <patrick.ridd@stgconsulting.com>
2020-07-06 17:14:50 -0700

    Updated the SignUpViewController to have the same functionality as the LoginViewController for the Apple Sign In button.

--- ScriptStarter/ViewControllers/Main VCs/SignUpViewController.swift
+++ ScriptStarter/ViewControllers/Main VCs/SignUpViewController.swift
@@ -14,8 +14,10 @@ import FBSDKLoginKit
 import GoogleSignIn
 import Firebase
 import MBProgressHUD
+import AuthenticationServices
+import CryptoKit
 
-class SignUpViewController: UIViewController, GIDSignInDelegate {
+class SignUpViewController: UIViewController, GIDSignInDelegate, ASAuthorizationControllerPresentationContextProviding {
 
     @IBOutlet weak var facebookButton: UIButton!
     @IBOutlet weak var googleSignInButton: GIDSignInButton!
@@ -25,7 +27,7 @@ class SignUpViewController: UIViewController, GIDSignInDelegate {
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var activityIndicatorContainerView: UIView!
-    
+    @IBOutlet weak var authenticationStackView: UIStackView!
     
     @IBOutlet weak var textFieldStackCenterYConstraint: NSLayoutConstraint!
     
@@ -37,7 +39,10 @@ class SignUpViewController: UIViewController, GIDSignInDelegate {
     @IBOutlet weak var signUpButtonBottomConstraint: NSLayoutConstraint!
     
     var loadingNotification = MBProgressHUD()
-
+   
+    // Unhashed nonce.
+    fileprivate var currentNonce: String?
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -50,8 +55,7 @@ class SignUpViewController: UIViewController, GIDSignInDelegate {
         
         // Google Sign-in
         GIDSignIn.sharedInstance().delegate = self
-   //     GIDSignIn.sharedInstance().uiDelegate = self
-        googleSignInButton.style = .iconOnly
+        googleSignInButton.style = .standard
         googleSignInButton.colorScheme = .light
         
         // Set TextFields Delegate
@@ -81,6 +85,12 @@ class SignUpViewController: UIViewController, GIDSignInDelegate {
             ]
         
         scriptBuilderLabel.attributedText = NSAttributedString(string: "Script Builder".localized, attributes: strokeTextAttributes)
+        
+        if #available(iOS 13.0, *) {
+            setupProviderLoginView()
+        } else {
+            // Fallback on earlier versions
+        }
     }
     
     func showActivityIndicator() {
@@ -313,4 +323,118 @@ class SignUpViewController: UIViewController, GIDSignInDelegate {
         }
     }
 
+}
+
+@available(iOS 13.0, *)
+extension SignUpViewController: ASAuthorizationControllerDelegate {
+    
+    func setupProviderLoginView() {
+        let authorizationButton = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn,
+                                                               authorizationButtonStyle: .whiteOutline)
+        authorizationButton.addTarget(self,
+                                      action: #selector(handleAuthorizationAppleIDButtonPress),
+                                      for: .touchUpInside)
+        self.authenticationStackView.addArrangedSubview(authorizationButton)
+    }
+    
+    @objc
+    func handleAuthorizationAppleIDButtonPress() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap { return String(format: "%02x", $0) }.joined()
+        
+        return hashString
+    }
+    
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        
+        var charset = Array<String.Element>()
+        let string = "0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._"
+        for char in string {
+            charset.append(char)
+        }
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      rawNonce: nonce)
+            // Sign in with Firebase.
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if let error = error {
+                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                    // you're sending the SHA256-hashed nonce as a hex string with
+                    // your request to Apple.
+                    print(error.localizedDescription)
+                    return
+                }
+                self.hideActivityIndicator(success: true)
+                self.presentScreenPlayCollection()
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+    }
 }
